fb69c1e674852e3fce0f76387c4050ed6716a776
dloppes <d.loppes.soares@gmail.com>
2022-06-27 09:12:32 +0000

    more tests created

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 
-const {game} = require("../game");
+const { getMaxListeners } = require("process");
+const {game, newGame, showScore} = require("../game");
 
 beforeAll(()=>{
     let fs = require("fs");
@@ -27,5 +28,27 @@ describe("game object contains correct keys", () =>{
     });
     test("choices container correct ids", ()=>{
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", ()=>{
+    beforeAll(()=>{
+        game.score = 42;
+        game.playerMoves =["button1", "button2"];
+        game.currentGame =["button1", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("sould set game score to zero", ()=>{
+        expect(game.score).toEqual(0);
+    });
+    test("sould clear the player moves array", ()=>{
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("sould clear the current Game array", ()=>{
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("sould display 0 for the element with id of score", ()=>{
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
